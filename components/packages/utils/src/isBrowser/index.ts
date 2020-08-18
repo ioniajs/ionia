6183c30ffd481ba29c1be2b4f7241a3b36dc968c
roylin <1002591652@qq.com>
2020-08-18 22:57:41 +0800

    refactor: components

--- components/packages/utils/src/isBrowser/index.ts
+++
@@ -1,7 +0,0 @@
-const isNode =
-  typeof process !== 'undefined' && process.versions != null && process.versions.node != null;
-
-const isBrowser = () =>
-  typeof window !== 'undefined' && typeof window.document !== 'undefined' && !isNode;
-
-export default isBrowser;
