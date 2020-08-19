fc7ecd762385d1c2fb73f0bce330d2f199440c23
RoyLin <1002591652@qq.com>
2020-08-19 11:21:13 +0800

    feat: added ionia components

--- apps/dashboard/src/pages/index.tsx
+++ apps/dashboard/src/pages/index.tsx
@@ -1,11 +1,15 @@
 import React, { Suspense } from 'react';
+import { Button } from '@ionia/components';
 
 const Trend = React.lazy(() => import('./components/Trend'));
 
 export default () => {
   return (
-    <Suspense fallback={<h1>loading...</h1>}>
-      <Trend />
-    </Suspense>
+    <div>
+      <Suspense fallback={<h1>loading...</h1>}>
+        <Trend />
+      </Suspense>
+      <Button />
+    </div>
   );
 };
---
+++ components/es/Button/index.js
@@ -0,0 +1,8 @@
+import "antd/es/button/style";
+import _Button from "antd/es/button";
+import React from "react";
+export default (function () {
+  return /*#__PURE__*/React.createElement(_Button, {
+    type: "primary"
+  }, "Ionia Button");
+});
