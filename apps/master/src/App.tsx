08c4e9bd9c74de6ec2f33301c8bc718251fbe175
RoyLin <1002591652@qq.com>
2020-09-07 17:10:33 +0800

    feat: master layout sider

--- apps/master/src/App.tsx
+++ apps/master/src/App.tsx
@@ -1,17 +1,34 @@
-import ProLayout from "@ant-design/pro-layout";
-import { BlankLayout, SlaveApp, useGlobalStore } from "@ionia/libs";
+import ProLayout, { SettingDrawer } from "@ant-design/pro-layout";
+import { BlankLayout, isDev, SlaveApp, useGlobalStore } from "@ionia/libs";
 import { IoniaApp } from "@ionia/libs/es/core/master-application";
 import React from "react";
 import { hot } from "react-hot-loader/root";
 import { useTranslation } from "react-i18next";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useHistory, useLocation } from "react-router-dom";
 import "./App.less";
 
 interface AppProps {}
 
+const defaultSettings: any = {
+  title: "Ionia",
+  logo:
+    "https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*1NHAQYduQiQAAAAAAAAAAABkARQnAQ",
+  navTheme: "dark",
+  layout: "side",
+  contentWidth: "Fluid",
+  fixedHeader: true,
+  headerHeight: 48,
+  siderWidth: 120,
+  primaryColor: "#1890ff",
+  splitMenus: false,
+  disableContentMargin: true,
+};
+
 const App: React.FC<AppProps> = () => {
   const { t } = useTranslation();
   const globalStore = useGlobalStore();
+  const history = useHistory();
+  const location = useLocation();
   return (
     <Switch>
       <Route path="/auth">
@@ -21,22 +38,22 @@ const App: React.FC<AppProps> = () => {
       </Route>
       <Route path="/">
         <ProLayout
+          {...defaultSettings}
           className="io-layout"
-          title="Ionia"
-          logo="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*1NHAQYduQiQAAAAAAAAAAABkARQnAQ"
-          disableContentMargin
-          siderWidth={120}
           route={{
-            path: "/",
-            routes: globalStore?.state?.apps
-              .filter((app: IoniaApp) => !app.hideInMenu)
-              .map((app: IoniaApp) => ({
-                path: app.activeRule,
-                name: app.name ? t(app.name) : "",
-              })),
+            routes: globalStore.state?.apps.map((app: IoniaApp) => ({
+              path: app.activeRule,
+              name: app.name ? t(app.name) : "",
+              hideInMenu: app.hideInMenu,
+            })),
           }}
+          menuItemRender={(m: { name: string; path: string }) => (
+            <div onClick={() => history.push(m.path)}>{m.name}</div>
+          )}
+          menuProps={{ selectedKeys: [location.pathname] }}
         >
           <SlaveApp />
+          {isDev && <SettingDrawer />}
         </ProLayout>
       </Route>
     </Switch>
