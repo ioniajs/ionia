7f05064d037c55138fddd4fa589269e299efab51
roylin <1002591652@qq.com>
2020-10-28 16:36:53 +0800

    fix: generateMenus

--- apps/master/src/components/MasterMenu/index.tsx
+++ apps/master/src/components/MasterMenu/index.tsx
@@ -1,13 +1,13 @@
 import { useGlobalStore } from '@ionia/libs';
 import { Menu } from 'antd';
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import routesMap, { IoniaMenuRoute } from '../../routes';
 import './index.less';
 
 const { SubMenu } = Menu;
 
-const generateMenus = (menuData: IoniaMenuRoute[]): any => {
+const generateMenus = (menuData: IoniaMenuRoute[]): ReactNode => {
 	if (menuData && menuData.length > 0) {
 		return menuData.map(item => {
 			if (item.children && item.children.length > 0) {
@@ -24,6 +24,7 @@ const generateMenus = (menuData: IoniaMenuRoute[]): any => {
 			);
 		});
 	}
+	return null;
 };
 
 const MasterMenu = () => {
