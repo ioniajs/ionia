488823f852d062bb490f52d417e4d090b16322cf
unknown <2472060859@qq.com>
2020-11-04 08:41:13 +0800

    '音频上传'

--- libs/src/components/Audio/index.tsx
+++ libs/src/components/Audio/index.tsx
@@ -8,6 +8,10 @@ import {
 import { Spin, Upload, Button } from 'antd';
 import { UploadChangeParam, UploadProps } from 'antd/lib/upload';
 import { UploadFile } from 'antd/lib/upload/interface';
+// import { PlayingButton } from 'react-simple-audio';
+import ReactAudioPlayer from 'react-audio-player';
+// import { PlayButton } from 'react-soundplayer/components';
+// import { withSoundCloudAudio } from 'react-soundplayer/addons';
 import React, { useState } from 'react';
 
 interface AudioProps extends UploadProps {
@@ -22,6 +26,25 @@ interface AudioProps extends UploadProps {
 }
 
 const { Dragger } = Upload;
+
+const clientId = 'YOUR CLIENT ID';
+const resolveUrl = 'https://soundcloud.com/ksmtk/chronemics';
+
+// const Player = withSoundCloudAudio((props: any) => {
+// 	let { track, currentTime } = props;
+
+// 	return (
+// 		<div className='custom-player'>
+// 			<PlayButton
+// 				className='custom-player-btn'
+// 				onPlayClick={() => {
+// 					console.log('play button clicked!');
+// 				}}
+// 				{...props}
+// 			/>
+// 		</div>
+// 	);
+// });
 
 export const Audio: React.FC<AudioProps> = props => {
 	const [loading, setLoading] = useState<boolean>(false);
@@ -38,7 +61,7 @@ export const Audio: React.FC<AudioProps> = props => {
 		showRemoveIcon: true,
 		removeIcon: <DeleteOutlined onClick={e => console.log(e, 'custom removeIcon event')} />,
 	};
-	const defaultFileList = [
+	const defaultFileList: any[] = [
 		{
 			uid: '1',
 			name: 'xxx.mp3',
@@ -80,28 +103,36 @@ export const Audio: React.FC<AudioProps> = props => {
 		...reset
 	} = props;
 	return (
-		<Upload
-			showUploadList={showUploadList}
-			defaultFileList={defaultFileList}
-			iconRender={(file: UploadFile) => {
-				// 可以通过是否点击播放来判断使用什么图片，播放时使用动态图，暂停时使用回形针静态图
-				if (played) {
-					return (
-						<>
-							<FastBackwardOutlined />
-						</>
-					);
-				}
-				return <PaperClipOutlined />;
-			}}
-			//   action={action}
-			onChange={onChange}
-			{...reset}
-		>
-			<Button className='ant-upload-drag-icon' type='primary'>
-				<UploadOutlined />
-				上传音频
-			</Button>
-		</Upload>
+		<div>
+			<ReactAudioPlayer src='my_audio_file.ogg' autoPlay controls />
+			{/* <Player
+				clientId={clientId}
+				resolveUrl={resolveUrl}
+				onReady={() => console.log('track is loaded!')}
+			/> */}
+			<Upload
+				showUploadList={showUploadList}
+				defaultFileList={defaultFileList}
+				iconRender={(file: UploadFile) => {
+					// 可以通过是否点击播放来判断使用什么图片，播放时使用动态图，暂停时使用回形针静态图
+					if (played) {
+						return (
+							<>
+								<FastBackwardOutlined />
+							</>
+						);
+					}
+					return <PaperClipOutlined />;
+				}}
+				//   action={action}
+				onChange={onChange}
+				{...reset}
+			>
+				<Button className='ant-upload-drag-icon' type='primary'>
+					<UploadOutlined />
+					上传音频
+				</Button>
+			</Upload>
+		</div>
 	);
 };
