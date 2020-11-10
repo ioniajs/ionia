ef15a4e9dd4ca8caea2fb2482f866057611006f0
饶志 <2829656235@qq.com>
2020-11-10 08:42:51 +0800

    feat: add navigation bar styles

--- apps/master/src/components/MasterNavTab/index.tsx
+++ apps/master/src/components/MasterNavTab/index.tsx
@@ -5,24 +5,26 @@ import { render } from 'react-dom';
 
 const { TabPane } = Tabs;
 
-
 export interface MasterNavTabProps {}
 
 const MasterNavTab = () => {
-		const tabContent = [
-			<span>{`Tab-${1}`}<i  className="iconfont icon-close" /></span>
-		]
-		
-		return (
-			<div className='io-master__nav-tab' >
-					<Tabs defaultActiveKey='1'>
-						{[...Array.from({ length: 30 }, (v, i) => i)].map(i => (
-							<TabPane tab={tabContent} key={i} disabled={i === 28}  />
-						))}
-					</Tabs>
-			</div>
-		);
-	}
+	const tabContent = [
+		<span>
+			{`Tab-${1}`}
+			<i className='iconfont icon-close' />
+		</span>,
+	];
+
+	return (
+		<div className='io-master__nav-tab'>
+			<Tabs defaultActiveKey='1'>
+				{[...Array.from({ length: 30 }, (v, i) => i)].map(i => (
+					<TabPane tab={tabContent} key={i} disabled={i === 28} />
+				))}
+			</Tabs>
+		</div>
+	);
+};
 {
 	/* <div className='io-master__nav-tab'>
 			<div className='extra'>
