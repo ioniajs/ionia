4db09a6a0490564684a214d5ac2ceeb9fa24a1d0
gujiwuqing <15279279713@163.com>
2020-11-13 14:39:44 +0800

    feat: merge

--- apps/master/src/components/MasterHeader/AvatarDropdown/index.tsx
+++ apps/master/src/components/MasterHeader/AvatarDropdown/index.tsx
@@ -8,23 +8,35 @@ export interface AvatarDropdownProps {}
 
 const AvatarDropdown: React.FC<AvatarDropdownProps> = () => {
 	const history = useHistory();
-	const onEdit =()=>{
-		console.log('123')
-	}
-	const onExit = ()=>{
-		history.replace('/auth')
-	}
+	const onEdit = () => {
+		console.log('123');
+	};
+	const onExit = () => {
+		history.replace('/auth');
+	};
 	const menus = (
 		<Menu className='user-info'>
 			<div className='info-header'>
 				<p className='info-title'>最近登录</p>
-				<p><i className='iconfont icon-time-circle'></i><span className='info-time'>2020-11-13</span></p>
-				<p><span>IP</span><span className='info-time'>120.12.12</span></p>
+				<p>
+					<i className='iconfont icon-time-circle'></i>
+					<span className='info-time'>2020-11-13</span>
+				</p>
+				<p>
+					<span>IP</span>
+					<span className='info-time'>120.12.12</span>
+				</p>
 			</div>
 			<Menu.Divider />
 			<div className='info-bottom'>
-				<p className='cursor' onClick={onEdit}><i className='iconfont icon-edit'></i><span>修改密码</span></p>
-				<p className='cursor' onClick={onExit}><i className='iconfont icon-dropout'></i><span>退出登录</span></p>
+				<p className='cursor' onClick={onEdit}>
+					<i className='iconfont icon-edit'></i>
+					<span>修改密码</span>
+				</p>
+				<p className='cursor' onClick={onExit}>
+					<i className='iconfont icon-dropout'></i>
+					<span>退出登录</span>
+				</p>
 			</div>
 		</Menu>
 	);
