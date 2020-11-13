1214233406483a8c564ff6587a3bc32ba3402aa5
gujiwuqing <15279279713@163.com>
2020-11-13 08:39:31 +0800

    perf: improved adding function

--- apps/cms/src/pages/User/components/Form/index.tsx
+++ apps/cms/src/pages/User/components/Form/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import { Button, message, Cascader, Form, Input } from 'antd';
-import ProForm, {
+import {
 	ModalForm,
-	ProFormText,
+
 	ProFormSelect,
-	ProFormSwitch,
+	ProFormSwitch, ProFormText
 } from '@ant-design/pro-form';
+import { Button, Cascader, Form, message } from 'antd';
+import React, { useState } from 'react';
 import './index.less';
 import { validateMessages } from './rule';
 
+interface BizFormProps {
+	title?: string;
+}
 const waitTime = (time: number = 100) => {
 	return new Promise(resolve => {
 		setTimeout(() => {
@@ -66,30 +69,38 @@ const residences = [
 	},
 ];
 
-// //确认密码校验一致
-// function handleCfmPwd(rules:any, value:string, callback:any) {
-// 	let loginpass = ModalForm.getFieldValue('cfmloginpass');
-// 	if (loginpass && loginpass !== value) {
-// 		callback(new Error('两次密码输入不一致'));
-// 	} else {
-// 		// Note: 必须总是返回一个 callback，否则 validateFieldsAndScroll 无法响应
-// 		callback();
-// 	}
-// }
-
-export default () => {
+// export default () => {
+export const BizForm: React.FC<BizFormProps> = (props) => {
+	const {
+		title
+	} = props;
 
 	const [form] = Form.useForm();
-
-	form.getFieldValue
-
+	const [visible, setVisible] = useState(false);
+	const onCancel = () => {
+		setVisible(false);
+	};
+	const onCreate = () => {
+		form.resetFields();
+	};
 	return (
 		<ModalForm
-		form={form}
+			form={form}
 			layout='horizontal'
-			title='新建用户'
+			// title='新建用户'
+			title={title}
 			width='530px'
-			trigger={<Button type='primary'>新建</Button>}
+			visible={visible}
+			trigger={
+				<Button
+					type='primary'
+					onClick={() => {
+						setVisible(true);
+					}}
+				>
+					新建
+				</Button>
+			}
 			onFinish={async values => {
 				await waitTime(2000);
 				console.log(values);
@@ -97,20 +108,28 @@ export default () => {
 				return true;
 			}}
 			validateMessages={validateMessages}
+			modalProps={{
+				onCancel,
+			}}
 			submitter={{
 				// 完全自定义整个区域
-				render: (props, doms) => {
-				  return (
-				<div className='btn-submitter'>
-					<Button type='default'  >取消</Button>
-					<Button type="primary"  >保存并分配权限</Button>
-					<Button type="primary"  >保存并继续新建</Button>
-					<Button type="primary"  htmlType="submit">保存</Button>
-				</div>
-				
-				  );
+				render: (_props, _doms) => {
+					return (
+						<div className='btn-submitter'>
+							<Button type='default' onClick={onCancel}>
+								取消
+							</Button>
+							<Button type='primary'>保存并分配权限</Button>
+							<Button type='primary' onClick={onCreate}>
+								保存并继续新建
+							</Button>
+							<Button type='primary' htmlType='submit'>
+								保存
+							</Button>
+						</div>
+					);
 				},
-			  }}
+			}}
 		>
 			<ProFormText
 				name='username'
@@ -128,14 +147,19 @@ export default () => {
 				name='confirm'
 				label='重复密码'
 				placeholder='请输入重复密码'
-				rules={[{ required: true }, { validator: (rule:any, value:string, callback:any) => {
-					const oldVal = form.getFieldValue('cipher')
-					if(oldVal!==value){
-						callback(new Error('两次密码输入不一致'));
-					}else{
-						callback()
-					}
-				 }}]}
+				rules={[
+					{ required: true },
+					{
+						validator: (_rule: any, value: string, callback: any) => {
+							const oldVal = form.getFieldValue('cipher');
+							if (oldVal !== value) {
+								callback(new Error('两次密码输入不一致'));
+							} else {
+								callback();
+							}
+						},
+					},
+				]}
 			/>
 			<Form.Item name='orgId' label='所属阵地' rules={[{ required: true }]}>
 				<Cascader options={residences} placeholder='请选择所属阵地' />
@@ -148,10 +172,46 @@ export default () => {
 				placeholder='请选择所属角色'
 			/>
 			<ProFormText name='realName' label='真实姓名' placeholder='请输入姓名' />
-			<ProFormText name='telephone' label='联系方式' placeholder='请输入联系方式' />
-			<ProFormText name='email' label='电子邮箱' placeholder='请输入电子邮箱' />
-			<ProFormSwitch name='status' label='用户状态' tooltip='禁用状态下的用户无法登录系统' initialValue={true} />
-			
+			<ProFormText
+				name='telephone'
+				label='联系方式'
+				placeholder='请输入联系方式'
+				rules={[
+					{
+						validator: (_rule: any, value: string, callback: any) => {
+							const regex = /^((\+)?86|((\+)?86)?)0?1[3458]\d{9}$/;
+							if (!value || regex.test(value)) {
+								callback();
+							} else {
+								callback('请输入正确的手机号码！');
+							}
+						},
+					},
+				]}
+			/>
+			<ProFormText
+				name='email'
+				label='电子邮箱'
+				placeholder='请输入电子邮箱'
+				rules={[
+					{
+						validator: (_rule: any, value: string, callback: any) => {
+							const reg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+							if (!value || reg.test(value)) {
+								callback();
+							} else {
+								callback('请输入正确的邮箱！');
+							}
+						},
+					},
+				]}
+			/>
+			<ProFormSwitch
+				name='status'
+				label='用户状态'
+				tooltip='禁用状态下的用户无法登录系统'
+				initialValue={true}
+			/>
 		</ModalForm>
 	);
 };
