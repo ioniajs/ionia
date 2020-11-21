b913a2c2eabf2514db6cbe4d69d2b50aacbfaaec
饶志 <2829656235@qq.com>
2020-11-21 17:45:34 +0800

    feat: marge

--- apps/cms/src/pages/PracticeBase/index.tsx
+++ apps/cms/src/pages/PracticeBase/index.tsx
@@ -10,83 +10,84 @@ export interface TableListItem {
 	name: string;
 }
 
-const positionRemove = async (ids: IdsDTO) => {
-	const removeRes = await deletePosition(ids);
-	if (removeRes.code !== 200) {
-		message.error('删除失败');
-	} else {
-		message.success('删除成功');
-	}
-	return removeRes.code;
-};
-const actionRef = useRef<ActionType>();
-const columns: ProColumns<OrgVO>[] = [
-	{
-		title: '阵地名称',
-		key: 'name',
-		dataIndex: 'name',
-		width: 400,
-	},
-	{
-		title: '阵地编号',
-		key: 'code',
-		dataIndex: 'code',
-		width: 175,
-	},
-	{
-		title: '阵地类型',
-		key: 'type',
-		dataIndex: 'type',
-		width: 215,
-	},
-	{
-		title: '地址',
-		key: 'address',
-		dataIndex: 'address',
-		width: 350,
-	},
-	{
-		title: '排序值',
-		width: 100,
-		render: (_, row) => <Input />,
-	},
-	{
-		title: '操作',
-		key: 'operation',
-		dataIndex: 'operation',
-		width: 50,
-		render: (_, row) => (
-			<a
-				onClick={async () => {
-					if (row) {
-						Modal.confirm({
-							title: '是否确定删除',
-							okText: '确定',
-							cancelText: '取消',
-							onOk: async () => {
-								const success = await positionRemove({
-									ids: [row.id],
-								});
-								if (success === 200) {
-									if (success === 200 && actionRef.current) {
-										actionRef.current.reload();
-									}
-								}
-							},
-						});
-					}
-				}}
-			>
-				删除
-			</a>
-		),
-	},
-];
-
 export default () => {
+	const positionRemove = async (ids: IdsDTO) => {
+		const removeRes = await deletePosition(ids);
+		if (removeRes.code !== 200) {
+			message.error('删除失败');
+		} else {
+			message.success('删除成功');
+		}
+		return removeRes.code;
+	};
+	const [selectedRowKeys, setSelectedRowKeys] = useState<number[]>([]);
+	const actionRef = useRef<ActionType>();
+	const columns: ProColumns<OrgVO>[] = [
+		{
+			title: '阵地名称',
+			key: 'name',
+			dataIndex: 'name',
+			width: 400,
+		},
+		{
+			title: '阵地编号',
+			key: 'code',
+			dataIndex: 'code',
+			width: 175,
+		},
+		{
+			title: '阵地类型',
+			key: 'type',
+			dataIndex: 'type',
+			width: 215,
+		},
+		{
+			title: '地址',
+			key: 'address',
+			dataIndex: 'address',
+			width: 350,
+		},
+		{
+			title: '排序值',
+			width: 100,
+			render: (_, row) => <Input />,
+		},
+		{
+			title: '操作',
+			key: 'operation',
+			dataIndex: 'operation',
+			width: 50,
+			render: (_, row) => (
+				<a
+					onClick={async () => {
+						if (row) {
+							Modal.confirm({
+								title: '是否确定删除',
+								okText: '确定',
+								cancelText: '取消',
+								onOk: async () => {
+									const success = await positionRemove({
+										ids: [row.id],
+									});
+									if (success === 200) {
+										if (success === 200 && actionRef.current) {
+											actionRef.current.reload();
+										}
+									}
+								},
+							});
+						}
+					}}
+				>
+					删除
+				</a>
+			),
+		},
+	];
 	return (
 		<BizPage showActions={false}>
 			<BizTable
+				rowKey='id'
 				actionRef={actionRef}
 				renderActions={() => (
 					<>
@@ -108,7 +109,14 @@ export default () => {
 						data: data.data,
 					}));
 				}}
-				rowSelection={{}}
+				rowSelection={{
+					selectedRowKeys,
+					onChange: selectedRowKeys => {
+						console.log(selectedRowKeys, 'ssss');
+						setSelectedRowKeys(selectedRowKeys as number[]);
+					},
+					checkStrictly: false,
+				}}
 				pagination={false}
 			/>
 		</BizPage>
