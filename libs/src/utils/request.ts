29a5d09b9f43c5ebb44d022a87f49b4ba3c6c955
roylin <1002591652@qq.com>
2020-12-02 15:33:53 +0800

    fix: judging api host according to environment

--- libs/src/utils/request.ts
+++ libs/src/utils/request.ts
@@ -1,9 +1,10 @@
-import { configs } from '../configs';
 import { extend } from 'umi-request';
-import { isDev } from '.';
+import { configs } from '../configs';
 
 const request = extend({
-	prefix: `${isDev ? '' : configs.API_HOST}${configs.API_PREFIX}`,
+	prefix: `${process.env.NODE_ENV === 'development' ? '' : configs.API_HOST}${
+		configs.API_PREFIX
+	}`,
 	timeout: 10000,
 	headers: {
 		'Accept-Language': 'zh-CN',
