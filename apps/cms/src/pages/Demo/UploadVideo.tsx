4c4269246aaadb47b1e177137dc4ac86ea6047af
unknown <2472060859@qq.com>
2020-12-08 08:59:24 +0800

    feat: videoupload

--- apps/cms/src/pages/Demo/UploadVideo.tsx
+++ apps/cms/src/pages/Demo/UploadVideo.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { VideoUpload } from '@ionia/libs';
 
 export default () => {
-	return <VideoUpload title='主视频' limit={3} tips='如有帮助信息，在此处展示' />;
+	return <VideoUpload title='主视频' tips='如有帮助信息，在此处展示' />;
 };
