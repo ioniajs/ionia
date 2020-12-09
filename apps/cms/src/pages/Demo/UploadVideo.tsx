d17e25a2d986a89b7394f0764ecd963482713ce9
unknown <2472060859@qq.com>
2020-12-09 17:43:50 +0800

    feat: annex upload

--- apps/cms/src/pages/Demo/UploadVideo.tsx
+++ apps/cms/src/pages/Demo/UploadVideo.tsx
@@ -5,9 +5,11 @@ import { VideoUpload, AudioUpload, AnnexUpload } from '@ionia/libs';
 // 	return <VideoUpload title='主视频' tips='如有帮助信息，在此处展示' />;
 // };
 export default () => {
-	return (<>
-		<VideoUpload title='主视频' tips='如有帮助信息，在此处展示' />
-		<AnnexUpload />
-		{/* <AudioUpload /> */}
-	</>);
+	return (
+		<>
+			<VideoUpload title='主视频' tips='如有帮助信息，在此处展示' />
+			<AnnexUpload />
+			{/* <AudioUpload /> */}
+		</>
+	);
 };
