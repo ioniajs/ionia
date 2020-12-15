39f30bff60cefe89e9800cbde852e13db8677a78
fwh <15279279713@163.com>
2020-12-15 08:34:24 +0800

    feat: menu pages

--- apps/cms/src/pages/Role/Detail/Authority/menu.tsx
+++ apps/cms/src/pages/Role/Detail/Authority/menu.tsx
@@ -1,5 +1,5 @@
-import { logger, roleMenuShow } from '@ionia/libs';
-import { useSet, useRequest } from 'ahooks';
+import { logger, roleMenuShow, MenuAuthVO } from '@ionia/libs';
+import { useRequest } from 'ahooks';
 import { Affix, Button, Checkbox, Col, Collapse, Row, Tooltip } from 'antd';
 import React, { useState } from 'react';
 import './index.less';
@@ -9,159 +9,184 @@ const { Panel } = Collapse;
 // const dataList: any[] = [];
 
 export default ({ roleId }: any) => {
-	// const [checkedKeys, setCheckedKeys] = useState<Set<string>>(new Set());
-	const [checkedKeys, { add, has, remove, reset }] = useSet<string[]>([]);
-	const [allKeys, setAllKeys] = useState<string[]>([]);
-	const { data } = useRequest(() => roleMenuShow({ roleId }), {
+	const [tree, setTree] = useState<MenuAuthVO[]>([]);
+	useRequest(() => roleMenuShow({ roleId }), {
 		onSuccess: data => {
-			const ids = getDefaultData(data.data);
-			setAllKeys(ids);
+			setTree(data.data);
 		},
 	});
 
 	//过滤数据 获取可选的数据
-	const getAvailableData = (list: any) => {
-		return list
-			.filter((u: any) => u.operatingFlag == 1)
-			.map((item: any) => {
-				if (item.children) {
-					item.children = getAvailableData(item.children);
-				}
-				return item;
-			});
+	const getAvailableData = (list: any, ids: number[] = []) => {
+		list.filter((u: any) => u.operatingFlag == 1).map((item: any) => {
+			ids.push(item.permissionFlag);
+			if (item.children) {
+				getAvailableData(item.children, ids);
+			}
+		});
+		return ids;
 	};
 
 	// 一级判断子级是否全部选中
 
 	//二级判断子级是否全部选中 ==>半选
 	const isChildrenAll = (data: any) => {
-		if (data.children && data.children.length > 0) {
-			let flag1;
-			let flag2;
-			let list = data.children.filter((t: any) => t.operatingFlag == 1);
-			list.map((o: any) => {
-				if (!has(o.key)) {
-					flag1 = true;
-				} else {
-					flag2 = true;
-				}
-			});
-			return !!(flag2 && flag1);
+		let flag: boolean = false;
+		if (data.children) {
+			const loop = (parent: any) => {
+				let ids: any[] = [];
+				parent.map((item: any) => {
+					if (item.operatingFlag == 1) {
+						ids.push(item);
+					}
+					let flag1 = ids.filter(t => t.permissionFlag == 1);
+					let flag2 = ids.filter(t => t.permissionFlag == 0);
+					if (flag1.length > 0 && flag2.length > 0) {
+						flag = true;
+					}
+				});
+			};
+			loop(data.children);
+			return flag;
 		} else {
 			return false;
 		}
 	};
 
-	//判断选中子级
+	//判断是否全部选中子级
 	const childrenAll = (data: any) => {
-		if (data.children && data.children.length > 0) {
-			let flag: boolean = true;
-			const loop = (data: any) => {
-				let list = data.children.filter((t: any) => t.operatingFlag == 1);
-				list.map((o: any) => {
-					if (!has(o.key)) {
+		let flag: boolean = true;
+		if (data.children) {
+			const loop = (parent: any) => {
+				parent.map((item: any) => {
+					if (item.operatingFlag == 1 && item.permissionFlag == 0) {
 						flag = false;
 					}
 				});
-				if (list.children && list.children.length) {
-					list.children.map((item: any) => {
-						loop(item);
-					});
-				}
 			};
-
-			loop(data);
+			loop(data.children);
 			return flag;
 		} else {
-			return has(data.key);
+			return data.permissionFlag == 1;
 		}
 	};
 
-	// 初始加载数据
-	const getDefaultData = (data: any, ids: string[] = []) => {
-		data.forEach((item: any) => {
-			if (item.permissionFlag == 1) {
-				add(item.key);
+	//最子级选中
+	const selectSelf = (self: any, parent: any, ancestor: any, flag: boolean) => {
+		flag ? (self.permissionFlag = 1) : (self.permissionFlag = 0);
+		if (parent) {
+			let flag1 = parent.children
+				.filter((t: any) => t.operatingFlag == 1)
+				.filter((i: any) => i.permissionFlag == 1);
+			if (flag1.length > 0) {
+				parent.permissionFlag = 1;
+			} else {
+				parent.permissionFlag = 0;
 			}
-			if (item.operatingFlag == 1) {
-				ids.push(item.key);
+		}
+		if (ancestor.children) {
+			const ids = getAvailableData(ancestor.children);
+			let flag2 = ids
+				.filter((t: any) => t.operatingFlag == 1)
+				.filter((i: any) => i.permissionFlag == 1);
+			if (flag2.length > 0) {
+				ancestor.permissionFlag = 1;
+			} else {
+				ancestor.permissionFlag = 0;
 			}
-			if (item.children && item.children.length > 0) {
-				getDefaultData(item.children, ids);
-			}
-		});
-		return ids;
+		}
+		setTree([...tree]);
 	};
 
-	const selectSelf = (self: any, parent: any, ancestor: any) => {
-		logger.debug('self', self);
-		// logger.debug('parent', parent);
-		// logger.debug('ancestor', ancestor);
-		if (has(self)) {
-			remove(self);
+	const checkedAll = (list: any, flag: boolean) => {
+		if (list.children) {
+			const loop = (data: any) => {
+				data.map((item: any) => {
+					if (item.operatingFlag == 1) {
+						if (flag) {
+							item.permissionFlag = 1;
+							list.permissionFlag = 1;
+						} else {
+							item.permissionFlag = 0;
+							list.permissionFlag = 0;
+						}
+					}
+					if (item.children) {
+						loop(item.children);
+					}
+				});
+			};
+			loop(list.children);
 		} else {
-			add(self);
+			flag ? (list.permissionFlag = 1) : (list.permissionFlag = 0);
 		}
-		const flag = decideParent(parent.children);
-		console.log('flag', flag, checkedKeys);
+		setTree([...tree]);
 	};
 
-	const decideParent = (list: any) => {
-		let flag = true;
-		const loop = (data: any) => {
-			data.map((t: any) => {
-				if (!has(t.key)) {
+	const isCheckAllFirst = (data: any) => {
+		let flag: boolean = true;
+		if (data.children) {
+			const ids = getAvailableData(data.children);
+			ids.map(item => {
+				if (item == 0) {
 					flag = false;
 				}
 			});
-		};
-		loop(list);
-		return flag;
-	};
-
-	const selectChildren = (list: any) => {
-		if (list.children && list.children.length > 0) {
+			return flag;
 		} else {
-			if (has(list.key)) {
-				remove(list.key);
-			} else {
-				add(list.key);
-			}
+			return data.permissionFlag == 1;
 		}
 	};
-
+	const checkNoAllFirst = (data: any) => {
+		let flag: boolean = false;
+		if (data.children) {
+			const ids = getAvailableData(data.children);
+			let arr1 = ids.filter(t => t == 1);
+			let arr2 = ids.filter(t => t == 0);
+			if (arr1.length > 0 && arr2.length > 0) {
+				flag = true;
+			}
+			return flag;
+		} else {
+			return false;
+		}
+	};
+	const checkAll = (data: any) => {
+		let flag: boolean = true;
+		const ids = getAvailableData(data);
+		console.log(ids);
+		ids.map(item => {
+			if (item == 0) {
+				flag = false;
+			}
+		});
+		return flag;
+	};
+	const isCheckAll = (data: any) => {
+		let flag: boolean = false;
+		const ids = getAvailableData(data);
+		let arr1 = ids.filter(t => t == 1);
+		let arr2 = ids.filter(t => t == 0);
+		if (arr1.length > 0 && arr2.length > 0) {
+			flag = true;
+		}
+		return flag;
+	};
 	return (
 		<>
 			<Affix offsetTop={100}>
-				<Button
-					type='primary'
-					onClick={() => {
-						console.log(checkedKeys);
-					}}
-				>
+				<Button type='primary' onClick={() => {}}>
 					保存
 				</Button>
 			</Affix>
 			<div className='io-cms-role-authority-site_check'>
-				<Checkbox
-					onChange={e => {
-						allKeys.map((item: any) => {
-							if (e.target.checked) {
-								add(item);
-							} else {
-								remove(item);
-							}
-						});
-					}}
-					indeterminate={allKeys.length != checkedKeys.size}
-					checked={allKeys.length == checkedKeys.size && checkedKeys.size > 0}
-				>
+				<Checkbox checked={checkAll(tree)} indeterminate={isCheckAll(tree)}>
 					全选
 				</Checkbox>
 				<Checkbox onChange={() => {}}>全部展开</Checkbox>
 			</div>
 			<Collapse className='io_cms_role_authority-menu_collapse'>
-				{data?.data?.map((item: any) => {
+				{tree.map((item: any) => {
 					return (
 						// @ts-ignore
 						<Panel
@@ -173,12 +198,12 @@ export default ({ roleId }: any) => {
 								>
 									<Checkbox
 										key={item.key}
-										onChange={e => {
-											selectChildren(item);
-										}}
 										disabled={item.operatingFlag == 0}
-										// indeterminate={isChildrenAll(item)}
-										checked={childrenAll(item)}
+										onChange={e => {
+											checkedAll(item, e.target.checked);
+										}}
+										checked={isCheckAllFirst(item)}
+										indeterminate={checkNoAllFirst(item)}
 									>
 										{item.name}
 									</Checkbox>
@@ -187,7 +212,7 @@ export default ({ roleId }: any) => {
 							showArrow={item.children?.length > 0}
 							key={item.key}
 						>
-							<Row gutter={[24, 24]}>
+							<Row gutter={[16, 16]}>
 								{item.onlyTwo == 0 &&
 									item.children?.map((i: any) => {
 										return (
@@ -198,17 +223,20 @@ export default ({ roleId }: any) => {
 											>
 												<div className='io_cms_role_authority-menu_collapse-panel_name'>
 													<Checkbox
-														onChange={e => {
-															selectChildren(i);
-														}}
 														disabled={i.operatingFlag == 0}
 														indeterminate={isChildrenAll(i)}
 														checked={childrenAll(i)}
+														onChange={e => {
+															checkedAll(i, e.target.checked);
+														}}
 													>
 														{i.name}
 													</Checkbox>
 												</div>
-												<Row className='io-cms-role-authority-menu_content'>
+												<Row
+													className='io-cms-role-authority-menu_content'
+													gutter={[16, 16]}
+												>
 													{i.children?.map((o: any) => {
 														return (
 															<Col
@@ -216,15 +244,15 @@ export default ({ roleId }: any) => {
 																className='io-cms-role-authority-menu_item'
 															>
 																<Checkbox
-																	checked={has(o.key)}
+																	checked={o.permissionFlag == 1}
 																	//点击的时候 判断是否有父级 有父级将父级加进去 没有则不加 移除的时候判断其他是否移除 有移除就将父级移除
 																	onChange={e => {
-																		selectSelf(o.key, i, item);
-																		// if (has(o.key)) {
-																		// 	remove(o.key);
-																		// } else {
-																		// 	add(o.key);
-																		// }
+																		selectSelf(
+																			o,
+																			i,
+																			item,
+																			e.target.checked
+																		);
 																	}}
 																	key={o.key}
 																	disabled={o.operatingFlag == 0}
@@ -250,13 +278,9 @@ export default ({ roleId }: any) => {
 												className='io-cms-role-authority-menu_item'
 											>
 												<Checkbox
-													checked={has(t.key)}
+													checked={t.permissionFlag == 1}
 													onChange={e => {
-														if (has(t.key)) {
-															remove(t.key);
-														} else {
-															add(t.key);
-														}
+														selectSelf(t, item, '', e.target.checked);
 													}}
 													key={t.key}
 													disabled={t.operatingFlag == 0}
